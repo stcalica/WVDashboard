f93ba356875a589ead535e9d141f6eb425be6bf2
stcalica <stcalica@gmail.com>
2016-02-03 11:02:13 -0800

    okay templates are up and running!

--- dashboard/dashboard/views.py
+++ dashboard/dashboard/views.py
@@ -1,5 +1,8 @@
 from dashboard import app
+from flask import render_template
+
 
 @app.route('/')
 def index():
-	return 'Hello World!'
+	#can make dict of values and just return as a var in render_templates, lik good 'ol django
+	return render_template('index.html')
