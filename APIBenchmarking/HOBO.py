49cddf618f6462e5a561cf3521d7863b7c1bc2f3
stcalica <stcalica@gmail.com>
2016-02-11 16:39:06 -0800

    playing around

--- APIBenchmarking/HOBO.py
+++ APIBenchmarking/HOBO.py
@@ -8,10 +8,18 @@ Benchmarking for the HOBOlink REST API
 """ 
 #header in csv is seperated by a row of hyphens, so need to find row of hyphens and split it up
 req = requests.get("http://webservice.hobolink.com/rest/public/devices/10459715/data_files/latest/txt") 
-csv = req.content.split("----")
-print(csv) 
-#for line in req.content:
-#	print(line) 
-#in_txt = csv.reader(req.content)
+data = req.content.split("------------")
+
+print(data[1])
+
+#sensor_data = csv.reader(data[1])
+#create csv file with date as title
+#open the file then write out to it 
+
+
+#print sensor_data.list_dialects()
+
+#for row in sensor_data: 
+#	print row 
 
 #curl http://webservice.hobolink.com/public/devices/10459715/data_files/latest/txt > API.csv 
