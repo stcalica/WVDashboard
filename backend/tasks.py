ff66416aa8bfa0b5ad88e454645927bec817cc04
Live session user <kycalica@gmail.com>
2016-02-27 23:21:52 +0000

    making just one large task

--- backend/tasks.py
+++ backend/tasks.py
@@ -1,47 +1,39 @@
 from celery import Celery
-
+import time, filecmp 
 
 app = Celery('tasks', broker='')
-serials = [] #hobologgers serial numbers 
+serials = [ 10459715 ] #hobologgers serial numbers 
 
 @app.task
 def HOBOgrab():
-	#loop for each serial number 
-	req = requests.get("http://webservice.hobolink.com/rest/public/devices/10459715/data_files/latest/txt") 
-	#header in csv is seperated by a row of hyphens, so need to find row of hyphens and split it up
-	data = req.content.split("------------")
-	#now need to strip out extra quotations and returns and newlines 
-	data = data[1].strip('"').strip().split('\r\n')
-	#name each csv by serial number
-	f_latest = open("csvonly.csv", 'wt')
-	try:
-		w = csv.writer(f_latest)
-		data.pop()
-		w.writerow(["#", "PacificDateTime", "channel1", "channel2", "channel3", "channel4", "channel5", "channel6", "channel7", "channel8", "channel9", "channel10", "channel11", "channel12", "channel13", "channel14", "channel15"])
-		for line in data[1:]: 
-			w.writerow(line.split(",")) 
-	finally:
-		f.close()
+	for serial in serials: 
+		#loop for each serial number 
+		req = requests.get("http://webservice.hobolink.com/rest/public/devices/10459715/data_files/latest/txt") 
+		#header in csv is seperated by a row of hyphens, so need to find row of hyphens and split it up
+		data = req.content.split("------------")
+		#now need to strip out extra quotations and returns and newlines 
+		data = data[1].strip('"').strip().split('\r\n')
+		#name each csv by serial number
+		f_latest = open( serial+ "_" + time.time() + ".csv", 'wt')
+		try:
+			w = csv.writer(f_latest)
+			data.pop()
+			w.writerow(["#", "PacificDateTime", "channel1", "channel2", "channel3", "channel4", "channel5", "channel6", "channel7", "channel8", "channel9", "channel10", "channel11", "channel12", "channel13", "channel14", "channel15"])
+			for line in data[1:]: 
+				w.writerow(line.split(",")) 
+		finally:
+			f.close()
 
-	try:
-		r = csv.read(f_old) 
-	finally:
-	#diff against old file
-	#insert new lines into database 
-
+		try:
+			filename1 = serial +"_"+time.time()+".csv" 
+			filename2 = serial+"_"+(time.time()*60*60*60)+".csv"
+			recent  = csv.read(filename1, "r")
+			last = csv.read(filename2, "r")	 
+			new = filecmp.cmp("./"+filename1, "./"+filename2) 
+		finally:
+			recent.close()
+			last.close() 
+		#diff against old file
+		#insert new lines into database 
+	
 	return 
-
-@app.task
-def DataUpload():
-	try:
-	 	latest  = csv.reader(f_latest) 
-		previous = csv.reader(f_recent) 
-	
-	finally:
-		#diff latest and previous 
-		#write the extra lines out and query into the database 			
-
-	
-	return
-
-	  
