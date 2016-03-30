0527ac3d3b729ea72a8eec8764a87e471f2fe903
Bradley Singer <bwsinger@ucdavis.edu>
2016-03-29 18:22:27 -0700

    Get tasks.py working with celery

--- backend/tasks.py
+++ backend/tasks.py
@@ -2,8 +2,13 @@ import csv, time
 import requests #, psycopg2
 import os
 
+from celery import Celery #allow celery to run this as an app
+
 serials = [ 10459715 ] #hobologgers serial numbers
 
+app = Celery('tasks', broker='amqp://guest@localhost//')
+
+@app.task
 def main():
 	eat_first = 1	# a stupid bool that lets us eat the first line of each file we diff
 	current = time.strftime("%H") 
