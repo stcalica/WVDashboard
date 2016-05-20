3a17830a3a812c544675d63893e6c79446a7740f
Tuan Vu <tuavu@ucdavis.edu>
2016-05-20 00:59:51 -0700

    Finished first logger on buildings, logger_processing wip

--- backend/buildings.py
+++ backend/buildings.py
@@ -4,7 +4,10 @@ buildings = {
 			  "serials":[{
 					"10459715":{
 						"kitchen": [1,2,3],
-                        "lights":[4,5]
+                        "plugs": [4,5,6,7],
+                        "lights": [8,10],
+                        "solar": [9,11,12],
+                        "ev": [13,14]
 						}
 
 					}]
--- backend/logger_processing.py
+++ backend/logger_processing.py
@@ -12,9 +12,32 @@ for b in buildings:
             data = req.content.split("------------")
             #now need to strip out extra quotations and returns and newlines
             data = data[1].strip('"').strip().split('\r\n')
-            for usecase in usecases:
-                 print("usecase : %s" % (usecase)) #each
-                 print("using channels: \n")
+            #somewhere here need to split last few lines of files
+            #need to somewhere define default values also
+            #should do this across all data
+            for row in data[1:3]:
+                line = row.split(',')
+                #printing a new entry to database
+                #two possible ones?
+                print("IF NOT EXISTS (SELECT * FROM table WHERE building = '" + b + "' AND timestamp = '"
+                    + line[1] +"') INSERT INTO table(building, timestamp) VALUES (" + b +", " + line[1] + ");")
+                # print("INSERT INTO table(building, timestamp) VALUES("+ b +", " + line[1] +")"
+                #     + " WHERE NOT EXISTS ( SELECT * FROM table WHERE building = '" + b + "' AND timestamp = '" + line[1]+"';")
+                query = "UPDATE table SET "
+                for u in usecases:
+                    print("usecase: %s" % u)
+                    value = 0
+                    for c in usecases[u]:
+                        print("channel: %s" % c)
+                        value += float(line[c+1])
+                    print("total value is: %s" % value)
+                    query += u + "=" + u + "+" + str(value) + ", "
+
+                query = query[:-2]
+                query += " WHERE timestamp = '" + line[1] +"';"
+                print("query: %s" % query)
+                    # UPDATE table SET s.usecase[u]
+                        # print(row[c+2])
             #     channels = [for c in usecases[usecase] ]
         #         for channel in usecases[usecase]:
                     #  for line in data:
